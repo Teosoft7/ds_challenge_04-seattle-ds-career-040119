aa823e1228b52d0cb72dba146c73a62695d20650
Taeho Jeon <thjeon.wa@gmail.com>
2019-04-04 09:28:08 -0700

    add temparature convert functions

--- temperizer.py
+++ temperizer.py
@@ -9,4 +9,32 @@ def convert_f_to_c(temperature_f):
 
 def convert_c_to_f(temperature_c):
     """Convert Celsius to Fahrenheit."""
-    pass
+    temperature_f = temperature_c * (9/5) + 32
+    return temperature_f
+
+    """
+    # To Do
+    * `convert_c_to_k`
+    * `convert_f_to_k`
+    * `convert_k_to_c`
+    * `convert_k_to_f`
+    """
+def convert_c_to_k(temperature_c):
+    """Convert Celsius to Kevin."""
+    # 0°C + 273.15 = 273.15K
+    temperature_k = temperature_c + 273.15
+    return temperature_k
+
+def convert_f_to_k(temperature_f):
+    """Convert Celsius to Fahrenheit."""
+    temperature_k = convert_c_to_k(convert_f_to_c(temperature_f))
+    return temperature_k
+
+def convert_k_to_c(temperature_k):
+    """Convert Kevin to Celsius."""
+    temperature_c = temperature_k - 273.15
+    return temperature_c
+
+def convert_k_to_f(temperature_k):
+    temperatute_f = convert_c_to_f(convert_k_to_c(temperature_k))
+    return temperatute_f
